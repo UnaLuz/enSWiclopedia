7eda6a35b6cd003d5c9396ac91ad8948427837c2
Luz Cucarella <luz.cucarella97@gmail.com>
2022-02-20 20:51:27 -0300

    Add adapter and view holder for character list

---
+++ app/src/main/java/com/unaluzdev/enswiclopedia/ui/adapter/CharacterAdapter.kt
@@ -0,0 +1,29 @@
+package com.unaluzdev.enswiclopedia.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.unaluzdev.enswiclopedia.R
+import com.unaluzdev.enswiclopedia.data.CharacterModel
+
+class CharacterAdapter(private val characterList: List<CharacterModel>) :
+    RecyclerView.Adapter<CharacterViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+
+        return CharacterViewHolder(
+            layoutInflater.inflate(
+                R.layout.character_list_item,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
+        holder.render(characterList[position])
+    }
+
+    override fun getItemCount(): Int = characterList.size
+}
---
+++ app/src/main/java/com/unaluzdev/enswiclopedia/ui/adapter/CharacterViewHolder.kt
@@ -0,0 +1,15 @@
+package com.unaluzdev.enswiclopedia.ui.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.unaluzdev.enswiclopedia.data.CharacterModel
+import com.unaluzdev.enswiclopedia.databinding.CharacterListItemBinding
+
+class CharacterViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    private val binding = CharacterListItemBinding.bind(view)
+
+    fun render(characterModel: CharacterModel) {
+        binding.textViewCharacterName.text = characterModel.name
+    }
+}
