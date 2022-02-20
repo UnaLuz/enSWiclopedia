3bffe3753603a3a69af5a62806e65610962ed434
Luz Cucarella <luz.cucarella97@gmail.com>
2022-02-20 18:23:24 -0300

    Add styles and main screen design.
    Moved MainActivity to ui folder.
    Added fonts and colors for the themes.

--- app/src/main/java/com/unaluzdev/enswiclopedia/ui/MainActivity.kt
+++ app/src/main/java/com/unaluzdev/enswiclopedia/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.unaluzdev.enswiclopedia
+package com.unaluzdev.enswiclopedia.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.unaluzdev.enswiclopedia.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
