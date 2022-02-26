5b0fccfbebcc8e3164d65eb260d41b41ba41eadd
Luz Cucarella <luz.cucarella97@gmail.com>
2022-02-26 16:25:16 -0300

    Move base url to a constants file

--- app/src/main/java/com/unaluzdev/enswiclopedia/core/RetrofitHelper.kt
+++ app/src/main/java/com/unaluzdev/enswiclopedia/core/RetrofitHelper.kt
@@ -1,12 +1,13 @@
 package com.unaluzdev.enswiclopedia.core
 
+import com.unaluzdev.enswiclopedia.util.BASE_INFO_URL
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
     fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("https://swapi.py4e.com/api/")
+            .baseUrl(BASE_INFO_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
---
+++ app/src/main/java/com/unaluzdev/enswiclopedia/util/constants.kt
@@ -0,0 +1,4 @@
+package com.unaluzdev.enswiclopedia.util
+
+const val BASE_INFO_URL = "https://swapi.py4e.com/api/"
+const val BASE_IMAGE_URL = "https://starwars-visualguide.com/assets/img/"
