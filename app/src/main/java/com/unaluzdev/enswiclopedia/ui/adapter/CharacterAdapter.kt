f6063186901b0773a4fb73790394012c4eced42b
Luz Cucarella <luz.cucarella97@gmail.com>
2022-02-28 16:59:11 -0300

    Separate ViewHolder for Character Items and the Button item
    * When creating the ViewHolder, get the viewType (defined in constants)
    and choose a ViewHolder according to it.
    * Make sure the item is not null and it's the correct viewType to render
    it.
    * Get the correct viewType: null item represents where the "See more"
    button goes.
    * Create helper methods to add and move the button in the list.

--- app/src/main/java/com/unaluzdev/enswiclopedia/ui/adapter/CharacterAdapter.kt
+++ app/src/main/java/com/unaluzdev/enswiclopedia/ui/adapter/CharacterAdapter.kt
@@ -5,9 +5,13 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.unaluzdev.enswiclopedia.R
 import com.unaluzdev.enswiclopedia.domain.model.SWCharacter
+import com.unaluzdev.enswiclopedia.util.VIEW_TYPE_ITEM
+import com.unaluzdev.enswiclopedia.util.VIEW_TYPE_PROGRESS
 
-class CharacterAdapter(private val characterList: ArrayList<SWCharacter>) :
-    RecyclerView.Adapter<CharacterViewHolder>() {
+class CharacterAdapter(private val characterList: ArrayList<SWCharacter?>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private var viewHolder: LoadingViewHolder? = null
 
     fun addCharacters(characterList: ArrayList<SWCharacter>) {
         val position = this.characterList.lastIndex
@@ -16,21 +20,56 @@ class CharacterAdapter(private val characterList: ArrayList<SWCharacter>) :
         this.notifyItemRangeInserted(position, nNewItems)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-
-        return CharacterViewHolder(
-            layoutInflater.inflate(
-                R.layout.character_list_item,
-                parent,
-                false
-            )
-        )
+    fun addLoadingView(onClick: () -> Unit) {
+        characterList.add(null)
+        notifyItemInserted(characterList.lastIndex)
+        viewHolder?.setOnClickListener(onClick)
     }
 
-    override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
-        holder.render(characterList[position])
+    fun moveLoadView(currentPos: Int, onClick: () -> Unit) {
+        if (currentPos in 0..characterList.lastIndex) {
+            characterList.removeAt(currentPos)
+            notifyItemRemoved(characterList.size)
+            addLoadingView(onClick)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if (viewType == VIEW_TYPE_PROGRESS) {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val holder = LoadingViewHolder(
+                layoutInflater.inflate(
+                    R.layout.load_more,
+                    parent,
+                    false
+                )
+            )
+            viewHolder = holder
+            return holder
+        } else {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            return CharacterViewHolder(
+                layoutInflater.inflate(
+                    R.layout.character_list_item,
+                    parent,
+                    false
+                )
+            )
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        characterList[position]?.let {
+            if (holder.itemViewType == VIEW_TYPE_ITEM) {
+                (holder as CharacterViewHolder).render(it)
+            }
+        }
     }
 
     override fun getItemCount(): Int = characterList.size
+
+    override fun getItemViewType(position: Int): Int {
+        return if (characterList[position] == null) VIEW_TYPE_PROGRESS else VIEW_TYPE_ITEM
+    }
+
 }
