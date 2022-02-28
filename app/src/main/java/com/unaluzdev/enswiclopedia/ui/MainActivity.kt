c93b5e1d932ce9499c842612cdeda6c3851a6778
Luz Cucarella <luz.cucarella97@gmail.com>
2022-02-28 20:52:56 -0300

    Move the button to the end of the list after adding more characters
    * Show/hide progress bar while fetching data.
    * Set the span size of items according to it's type.
    * Add dummy loadMore() function to execute when "See more" button is
    clicked.

--- app/src/main/java/com/unaluzdev/enswiclopedia/ui/MainActivity.kt
+++ app/src/main/java/com/unaluzdev/enswiclopedia/ui/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import com.unaluzdev.enswiclopedia.databinding.ActivityMainBinding
 import com.unaluzdev.enswiclopedia.ui.adapter.CharacterAdapter
+import com.unaluzdev.enswiclopedia.util.VIEW_TYPE_ITEM
+import com.unaluzdev.enswiclopedia.util.VIEW_TYPE_PROGRESS
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,24 +21,58 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initRecyclerView()
-        characterViewModel.onCreate()
 
         // Observers
         characterViewModel.characterList.observe(this) {
             val adapter = binding.recyclerViewCharacter.adapter as CharacterAdapter
             adapter.addCharacters(it)
+            adapter.moveLoadView(adapter.itemCount - 1) { loadMore() }
         }
-        characterViewModel.error.observe(this) { errorMsg ->
-            if (errorMsg.isNotBlank())
-                Toast.makeText(this, errorMsg, Toast.LENGTH_LONG)
+
+        characterViewModel.uiState.observe(this) { state ->
+            if (state.loading) binding.progressIndicator.show()
+            else binding.progressIndicator.hide()
+
+            if (!state.error.isNullOrBlank())
+                Toast.makeText(this, state.error, Toast.LENGTH_LONG)
                     .show()
         }
+
+        // Finish onCreate
+        characterViewModel.onCreate()
     }
 
     private fun initRecyclerView() {
-        binding.recyclerViewCharacter.layoutManager = GridLayoutManager(this, 2)
-        binding.recyclerViewCharacter.adapter =
-            CharacterAdapter(ArrayList())
+        with(binding.recyclerViewCharacter) {
+            // Create layout manager and adapter
+            val layoutManager = GridLayoutManager(this@MainActivity, 2)
+            val adapter = CharacterAdapter(ArrayList())
+            // Initialize them in the recycler view
+            this.layoutManager = layoutManager
+            this.adapter = adapter
+            addCustomSpanSizeLookup(layoutManager, adapter)
+            adapter.addLoadingView { loadMore() }
+        }
+    }
+
+    private fun addCustomSpanSizeLookup(
+        layoutManager: GridLayoutManager,
+        adapter: CharacterAdapter
+    ) {
+        layoutManager.spanSizeLookup =
+            object : GridLayoutManager.SpanSizeLookup() {
+                override fun getSpanSize(position: Int): Int {
+                    return when (adapter.getItemViewType(position)) { // A sealed class or enum would be better
+                        VIEW_TYPE_ITEM -> 1
+                        VIEW_TYPE_PROGRESS -> layoutManager.spanCount
+                        else -> -1
+                    }
+                }
+            }
+    }
+
+    private fun loadMore() {
+        Toast.makeText(this@MainActivity, "Loading more", Toast.LENGTH_SHORT).show()
     }
 
 }
