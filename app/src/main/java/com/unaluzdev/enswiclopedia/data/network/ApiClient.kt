b279ad14986236fd764f7d29369a9e8ce6f4f16f
Luz Cucarella <luz.cucarella97@gmail.com>
2022-05-08 17:33:13 -0300

    Adds a Planet Api Client

--- app/src/main/java/com/unaluzdev/enswiclopedia/data/network/ApiClient.kt
+++ app/src/main/java/com/unaluzdev/enswiclopedia/data/network/ApiClient.kt
@@ -1,8 +1,10 @@
 package com.unaluzdev.enswiclopedia.data.network
 
 import com.unaluzdev.enswiclopedia.data.model.PeopleResponse
+import com.unaluzdev.enswiclopedia.data.model.PlanetModel
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CharacterApiClient {
@@ -11,4 +13,11 @@ interface CharacterApiClient {
         @Query("page") page: Int? = null,
         @Query("search") query: String? = null
     ): Response<PeopleResponse>
+}
+
+interface PlanetApiClient {
+    @GET("planets/{id}/")
+    suspend fun getPlanet(
+        @Path("id") id: String
+    ): Response<PlanetModel>
 }
