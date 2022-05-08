6fac708be03b7eeee2d0e5cdc7e093d31125d4fc
Luz Cucarella <luz.cucarella97@gmail.com>
2022-05-08 17:50:51 -0300

    Use the CharacterDetailsViewModel and listen for state changes

--- app/src/main/java/com/unaluzdev/enswiclopedia/ui/fragment/CharacterDetailsFragment.kt
+++ app/src/main/java/com/unaluzdev/enswiclopedia/ui/fragment/CharacterDetailsFragment.kt
@@ -8,13 +8,13 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
+import com.unaluzdev.enswiclopedia.R
 import com.unaluzdev.enswiclopedia.databinding.FragmentCharacterDetailsBinding
 import com.unaluzdev.enswiclopedia.domain.model.SWCharacter
+import com.unaluzdev.enswiclopedia.ui.CharacterDetailsViewModel
 import com.unaluzdev.enswiclopedia.ui.CharacterViewModel
-
-private const val CHARACTER_ID = "id"
-private const val NAME = "name"
 
 class CharacterDetailsFragment : Fragment() {
     // Args
@@ -27,6 +27,7 @@ class CharacterDetailsFragment : Fragment() {
 
     // Shared ViewModel
     private val characterViewModel: CharacterViewModel by activityViewModels()
+    private val characterDetailsViewModel: CharacterDetailsViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,17 +47,39 @@ class CharacterDetailsFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        getCharacterById(characterID)?.render() ?: run {
-            Toast.makeText(requireContext(), "No character found", Toast.LENGTH_SHORT)
+
+        // Observers
+        characterDetailsViewModel.homeworldState.observe(viewLifecycleOwner) { state ->
+            if (state.loading) {
+                binding.homeworldInfo.text = getString(R.string.loading)
+            } else if (state.planet != null) {
+                binding.homeworldInfo.text = state.planet.name
+            }
+            state.error?.let { message ->
+                Toast.makeText(
+                    requireContext(),
+                    message,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        getCharacterById(characterID)?.apply {
+            renderStaticInfo()
+            loadDinamicInfo()
+        } ?: run {
+            Toast.makeText(
+                requireContext(),
+                getString(R.string.no_character_found),
+                Toast.LENGTH_SHORT
+            )
         }
     }
 
-    private fun SWCharacter.render() {
+    private fun SWCharacter.renderStaticInfo() {
         let { character ->
             with(binding) {
-                renderImageWith(this.characterImage, character.imgUrl)
                 birthDateInfo.text = character.birth_year
-                homeworldInfo.text = character.homeworld
                 hairColorInfo.text = character.hair_color
                 genderInfo.text = character.gender
                 eyeColorInfo.text = character.eye_color
@@ -66,6 +89,11 @@ class CharacterDetailsFragment : Fragment() {
                 filmsInfo.text = character.films.joinToString(", ")
             }
         }
+    }
+
+    private fun SWCharacter.loadDinamicInfo() {
+        renderImageWith(binding.characterImage, this.imgUrl)
+        characterDetailsViewModel.onLoadHomeworld(this.homeworld)
     }
 
     private fun renderImageWith(imageView: ImageView, url: String) {
@@ -78,6 +106,9 @@ class CharacterDetailsFragment : Fragment() {
         characterViewModel.getCharacter { it.id == characterID }
 
     companion object {
+        private const val CHARACTER_ID = "id"
+        private const val NAME = "name"
+
         const val TAG = "CharacterDetailsFragment"
 
         @JvmStatic
