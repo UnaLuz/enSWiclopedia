218fd6f9774ca9fe56f36a1cd81c35afb88aa03a
Luz Cucarella <luz.cucarella97@gmail.com>
2022-05-08 13:10:50 -0300

    Update helpers to use Categories instead of Strings

--- app/src/main/java/com/unaluzdev/enswiclopedia/util/helpers.kt
+++ app/src/main/java/com/unaluzdev/enswiclopedia/util/helpers.kt
@@ -1,9 +1,13 @@
 package com.unaluzdev.enswiclopedia.util
 
-fun getId(url: String, category: String = "people"): String? {
+enum class Category(val alias: String? = null) {
+    PEOPLE("characters"), PLANETS, SPECIES, FILMS
+}
+
+fun getId(url: String, category: Category = Category.PEOPLE): String? {
     val pattern = """${BASE_INFO_URL}$category/(\d+)/""".toRegex()
     return pattern.find(url)?.groupValues?.get(1)
 }
 
-fun getImageUrl(url: String, category: String = "characters"): String =
-    BASE_IMAGE_URL + "$category/${getId(url)}.jpg"
+fun getImageUrl(url: String, category: Category = Category.PEOPLE): String =
+    BASE_IMAGE_URL + "${category.alias ?: category}/${getId(url, category)}.jpg"
