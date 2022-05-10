d8e642e5469f5cc3c7a3f4aa7f053df2cf67868f
Luz Cucarella <luz.cucarella97@gmail.com>
2022-05-10 02:34:07 -0300

    Adds a placeholder layout and a recycler view for the species
    - Initialize the recycler view
    - Observe for species state changes
    - Notify the ViewModel to load species

--- app/src/main/java/com/unaluzdev/enswiclopedia/ui/fragment/CharacterDetailsFragment.kt
+++ app/src/main/java/com/unaluzdev/enswiclopedia/ui/fragment/CharacterDetailsFragment.kt
@@ -1,22 +1,22 @@
 package com.unaluzdev.enswiclopedia.ui.fragment
 
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
-import com.bumptech.glide.Glide
-import com.bumptech.glide.RequestBuilder
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.unaluzdev.enswiclopedia.R
 import com.unaluzdev.enswiclopedia.databinding.FragmentCharacterDetailsBinding
 import com.unaluzdev.enswiclopedia.domain.model.SWCharacter
 import com.unaluzdev.enswiclopedia.ui.CharacterDetailsViewModel
 import com.unaluzdev.enswiclopedia.ui.CharacterViewModel
+import com.unaluzdev.enswiclopedia.ui.adapter.SpeciesAdapter
+import com.unaluzdev.enswiclopedia.ui.helper.renderImageWith
 
 class CharacterDetailsFragment : Fragment() {
     // Args
@@ -49,6 +49,7 @@ class CharacterDetailsFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupSpeciesRecyclerView()
 
         // Observers
         characterDetailsViewModel.homeworldState.observe(viewLifecycleOwner) { state ->
@@ -57,28 +58,69 @@ class CharacterDetailsFragment : Fragment() {
             } else if (state.planet != null) {
                 binding.homeworldInfo.horizontalCardTextView.text = state.planet.name
                 renderImageWith(
+                    requireContext(),
                     binding.homeworldInfo.horizontalCardImageView,
                     state.planet.imgUrl
                 ) { circleCrop() }
             }
-            state.error?.let { message ->
-                Toast.makeText(
-                    requireContext(),
-                    message,
-                    Toast.LENGTH_SHORT
-                ).show()
+            showError(state.error)
+        }
+
+        characterDetailsViewModel.speciesState.observe(viewLifecycleOwner) { state ->
+            if (state.loading) {
+                showSpeciesPlaceholder(R.string.loading)
+            } else {
+                if (state.species.isNullOrEmpty()) {
+                    showSpeciesPlaceholder(R.string.unknown, showImage = false)
+                } else {
+                    hideSpeciesPlaceHolder()
+                    (binding.speciesInfo.adapter as SpeciesAdapter?)?.updateList(state.species!!)
+                }
             }
+            showError(state.error)
         }
 
         getCharacterById(characterID)?.apply {
             renderStaticInfo()
-            loadDinamicInfo()
+            loadDynamicInfo()
         } ?: run {
             Toast.makeText(
                 requireContext(),
                 getString(R.string.no_character_found),
                 Toast.LENGTH_SHORT
             )
+        }
+    }
+
+    private fun showSpeciesPlaceholder(@StringRes res: Int, showImage: Boolean = true) {
+        with(binding.speciesPlaceholder) {
+            root.visibility = View.VISIBLE
+            horizontalCardTextView.text = getString(res)
+            horizontalCardImageView.visibility = if (showImage) View.VISIBLE else View.GONE
+        }
+    }
+
+    private fun hideSpeciesPlaceHolder() {
+        with(binding.speciesPlaceholder) {
+            root.visibility = View.GONE
+            horizontalCardTextView.text = null
+            horizontalCardImageView.visibility = View.GONE
+        }
+    }
+
+    private fun setupSpeciesRecyclerView() {
+        binding.speciesInfo.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        binding.speciesInfo.adapter = SpeciesAdapter(emptyList())
+    }
+
+    private fun showError(error: String?) {
+        error?.let { message ->
+            Toast.makeText(
+                requireContext(),
+                message,
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
@@ -91,31 +133,15 @@ class CharacterDetailsFragment : Fragment() {
                 eyeColorInfo.text = character.eye_color
                 heightInfo.text = character.height
                 massInfo.text = character.mass
-                speciesInfo.text = character.species.joinToString(", ")
                 filmsInfo.text = character.films.joinToString(", ")
             }
         }
     }
 
-    private fun SWCharacter.loadDinamicInfo() {
-        renderImageWith(binding.characterImage, this.imgUrl)
+    private fun SWCharacter.loadDynamicInfo() {
+        renderImageWith(requireContext(), binding.characterImage, this.imgUrl)
         characterDetailsViewModel.onLoadHomeworld(this.homeworld)
-    }
-
-    private fun renderImageWith(
-        imageView: ImageView,
-        url: String,
-        options: (RequestBuilder<Drawable>.() -> RequestBuilder<Drawable>)? = null
-    ) {
-        Glide.with(requireContext())
-            .load(url)
-            .error(R.drawable.ic_image)
-            .also {
-                if (options != null) {
-                    it.options()
-                }
-            }
-            .into(imageView)
+        characterDetailsViewModel.onLoadSpecies(this.species)
     }
 
     private fun getCharacterById(characterID: Int?): SWCharacter? =
